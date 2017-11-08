ac925f3941da2a370cb96bdf97e6cee02a92d968
Coder-DanielHan <2420626787@qq.com>
2017-11-08 22:30:18 +0800

    未完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.Region;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -18,6 +19,7 @@ public class Practice02ClipPathView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Path circlePath1, circlePath2;
 
     public Practice02ClipPathView(Context context) {
         super(context);
@@ -33,13 +35,24 @@ public class Practice02ClipPathView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.maps);
+        circlePath1 = new Path();
+        circlePath1.addCircle(point1.x + 200, point1.y + 200, 150, Path.Direction.CW);
+        circlePath2 = new Path();
+        circlePath2.addCircle(point2.x + 200, point2.y + 200, 150, Path.Direction.CCW);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.clipPath(circlePath1);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        canvas.save();
+        canvas.clipPath(circlePath2, Region.Op.DIFFERENCE);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
